1298e7c0cc1c1bd3059e0cba9cad6e8d94a819f0
Milan Kolar <mail@mkolar.com>
2015-05-18 11:31:18 +0200

    removing pyblish_win imports

--- pyblish_houdini/houdini_path/scripts/123.py
+++ pyblish_houdini/houdini_path/scripts/123.py
@@ -5,8 +5,5 @@ except ImportError as e:
     print "pyblish: Could not load integration: %s" % e
 
 else:
-    from pyblish_win import util
-    util.augment_pythonpath()
-    util.augment_path()
     import pyblish_houdini
     pyblish_houdini.setup()
