aace3632b2784f4d81a5c43db0254d38dd94bd8a
Hannah Pichler <hannnnah.pichler@gmail.com>
2022-06-13 18:19:42 +0200

    Add classifier

--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ classifiers = [
     "Programming Language :: Python :: 2",
     "Programming Language :: Python :: 2.6",
     "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.7",
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Topic :: Utilities"
 ]
